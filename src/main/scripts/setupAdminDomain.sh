7958389e9e671c2fb4895ad195a76d98971de74a
gnsuryan <gnsuryan@users.noreply.github.com>
2021-04-15 10:58:40 +0000

    fix issue in passing ssl params

--- src/main/scripts/setupAdminDomain.sh
+++ src/main/scripts/setupAdminDomain.sh
@@ -479,7 +479,7 @@ function startTestServerAndValidateKeyStore()
    tempCustomTrustKeyStoreData=$(echo "$customTrustKeyStoreData" | base64 --decode)
 
 
-   export CERTVALIDATOR_JAR_EXEC_COMMAND="java -jar ${KEYSTORE_TEMP_PATH}/certvalidator.jar $tempIdentityKeyStoreType $tempCustomTrustKeyStoreData $customIdentityKeyStorePassPhrase $serverPrivateKeyPassPhrase $tempTrustKeyStoreType $tempCustomTrustKeyStoreData $customTrustKeyStorePassPhrase $customTrustKeyStorePassPhrase"
+   export CERTVALIDATOR_JAR_EXEC_COMMAND="java -jar ${KEYSTORE_TEMP_PATH}/certvalidator.jar \"$tempIdentityKeyStoreType\" \"$tempCustomIdentityKeyStoreData\" \"$customIdentityKeyStorePassPhrase\" \"$serverPrivateKeyPassPhrase\" \"$tempTrustKeyStoreType\" \"$tempCustomTrustKeyStoreData\" \"$customTrustKeyStorePassPhrase\" \"$customTrustKeyStorePassPhrase\""
 
    echo "COMMAND: $CERTVALIDATOR_JAR_EXEC_COMMAND"
 
